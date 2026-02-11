e0a1aa81c4495c617e2cc33fe251ab4c8553bdd1
CarolinaN-Ironhack-ACFT2026 <carolinanami@gmail.com>
2026-02-11 16:51:49 +0100

    Delete Desktop/Ironhack/Week 2/Day 3 directory

--- Desktop/Ironhack/Week 2/Day 3/my_first_usage.py
+++
@@ -1,5 +0,0 @@
-# Import only the functions you need
-from product_listing_utils import (
-    PromptGenerator,
-    FileHandler,
-)
--- Desktop/Ironhack/Week 2/Day 3/test_price.py
+++
@@ -1,6 +0,0 @@
-from product_listing_utils import format_price
-
-# Use it
-price = 99.99
-formatted = format_price(price)
-print(formatted)  # Output: $99.99
